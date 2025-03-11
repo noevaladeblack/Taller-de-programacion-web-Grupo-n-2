9763b5c0a9b1f96ada8fcf90f6faa28e7ad47382
amoncadadev <amoncada.dev@gmail.com>
2025-03-10 21:20:42 -0300

    Prometheus v1
    Primera pagina web

---
+++ Prometheus/conexion.php
@@ -0,0 +1,41 @@
+<?php
+//validamos datos del servidor
+$user = "root";
+$pass = "";
+$host = "localhost";
+$datab = "prometheus";
+
+
+//conetamos al base datos
+$connection = mysqli_connect($host, $user, $pass);
+
+//hacemos llamado al imput de formuario
+$name = $_POST["name"] ;
+$phone = $_POST["phone"] ;
+$email = $_POST["email"] ;
+$mensaje = $_POST["mensaje"] ;
+
+if(!$connection) {
+
+            echo "No se ha podido conectar con el servidor" . mysql_error();
+}else{
+
+    $db = mysqli_select_db($connection,$datab);
+
+    if (!$db){
+        echo "No se ha podido encontrar la Tabla";
+    }else{
+        $instruccion_SQL = "INSERT INTO contacto(Nombre, Fono, Email, Mensaje) 
+                        VALUES ('$name', '$phone', '$email', '$mensaje')";
+
+        $resultado = mysqli_query($connection,$instruccion_SQL);
+
+        mysqli_close( $connection );
+
+    }                      
+            
+}
+    
+   echo'<a href="in"> Volver Atrás</a>';
+
+?>
